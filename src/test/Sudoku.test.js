4e2554c86e495f3b99c9a10f504626006449ca91
A-SED-HASAN <hasanaghaei6@gmail.com>
2023-12-08 23:42:11 +0330

    test improved

--- src/test/Sudoku.test.js
+++ src/test/Sudoku.test.js
@@ -63,3 +63,15 @@ test('all notes for all cells must be null in the first ', () => {
 
 
 //TODO
+expect({ a: 1 }).toHaveProperty('a')
+expect({ a: 1 }).toHaveProperty('a', 1)
+expect({ a: { b: 1 } }).toHaveProperty('a.b')
+expect({ a: 1, b: 2 }).toMatchObject({ a: 1 })
+expect({ a: 1, b: 2 }).toMatchObject({
+  a: expect.any(Number),
+  b: expect.any(Number),
+})
+expect([{ a: 1 }, { b: 2 }]).toEqual([
+  expect.objectContaining({ a: expect.any(Number) }),
+  expect.anything(),
+])
